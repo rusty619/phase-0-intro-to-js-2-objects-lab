d7bdd849737797844de6ceb47706166b94bab873
rusty619 <rusty6916@gmail.com>
2022-03-17 10:15:37 -0400

    commit tree 9547451aa7582d8dab0be332a36a08e54b35bd72

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    var newEmployee = {...employee}
+    newEmployee[key] = value
+    return newEmployee
+}
